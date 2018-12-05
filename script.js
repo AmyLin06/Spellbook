7cee4b6c736fbb4c27cf8253c462c264227e47a0
AmyLin06 <43828844+AmyLin06@users.noreply.github.com>
2018-12-05 17:07:10 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,11 @@
+$(".first").click(function() {
+    $(".one").show();
+});
+
+$(".second").click(function() {
+    $(".two").slideDown();
+});
+
+$(".third").click(function() {
+    $(".three").fadeToggle();
+});
